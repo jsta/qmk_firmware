147b3546555a300b97a35863daae68dffd997a2d
jsta <jemma.stachelek@gmail.com>
2023-11-02 22:03:46 -0600

    porting mod and specials to colmak layer

--- keyboards/sofle/keymaps/jsta/keymap.c
+++ keyboards/sofle/keymaps/jsta/keymap.c
@@ -44,34 +44,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [0] = LAYOUT(
-  QK_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_SCLN, KC_QUOT,    KC_MINS,    KC_EQL,    KC_HOME,  KC_PGUP,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_END,  KC_PGDN,
-  MO(3),   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,     KC_P,  KC_BSPC,
-  SC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_F11,     KC_F12, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  SC_RSPC,
+  QK_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                    KC_SCLN, KC_QUOT,    KC_MINS,    KC_EQL,    KC_HOME,  KC_PGUP,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,       KC_I,       KC_O,      KC_END,   KC_PGDN,
+  MO(3),   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,       KC_K,       KC_L,      KC_P,     KC_BSPC,
+  SC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_F11,     KC_F12, KC_N,    KC_M,       KC_COMM,    KC_DOT,    KC_SLSH,  SC_RSPC,
                  KC_LCTL,KC_LGUI,KC_LALT, KC_ENT, MO(2),      MO(2), KC_SPC, KC_LBRC, KC_RBRC, KC_RCTL
 ),
 /*
  * COLEMAK
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   Q  |   W  |   F  |   P  |   G  |                    |   J  |   L  |   U  |   Y  |   ;  | Bspc |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | TAB  |   A  |   R  |   S  |   T  |   D  |-------.    ,-------|   H  |   N  |   E  |   I  |   O  |  '   |
- * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
- *            |      |      |      |      |/       /         \      \ |      |      |      |      |
- *            `-----------------------------------'           '------''---------------------------'
  */
 
 [1] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
-  KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
-  KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,      XXXXXXX,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                 KC_LGUI,KC_LALT,KC_LCTL,KC_TRNS, KC_ENT,      KC_SPC,  KC_TRNS, KC_RCTL, KC_RALT, KC_RGUI
+  QK_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_SCLN, KC_QUOT,    KC_MINS,    KC_EQL,    KC_HOME,  KC_PGUP,
+  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_END,   KC_PGDN,
+  MO(3),   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                       KC_H,    KC_N,    KC_E,    KC_I,    KC_O,   KC_BSPC,
+  SC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_F11,      KC_F12, KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  SC_RSPC,
+                 KC_LCTL,KC_LGUI,KC_LALT, KC_ENT, MO(2),      MO(2), KC_SPC, KC_LBRC, KC_RBRC, KC_RCTL
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
