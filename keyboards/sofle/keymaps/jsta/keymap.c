36ed0e35fc6ca7bc5295aaf9c4c485c680c54dfc
jsta <jemma.stachelek@gmail.com>
2024-01-06 21:05:50 -0700

    formatting

--- keyboards/sofle/keymaps/jsta/keymap.c
+++ keyboards/sofle/keymaps/jsta/keymap.c
@@ -44,11 +44,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [1] = LAYOUT(
-  QK_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                    KC_SCLN, KC_QUOT,    KC_MINS,    KC_EQL,    KC_HOME,  KC_PGUP,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,       KC_I,       KC_O,      KC_END,   KC_PGDN,
-  MO(3),   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,       KC_K,       KC_L,      KC_P,     KC_BSPC,
-  SC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_F11,     KC_F12, KC_N,    KC_M,       KC_COMM,    KC_DOT,    KC_SLSH,  SC_RSPC,
-                 KC_LCTL,KC_LGUI,KC_LALT, KC_ENT, MO(2),      MO(2), KC_SPC, KC_LBRC, KC_RBRC, KC_RCTL
+  QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5,                 KC_SCLN,  KC_QUOT, KC_MINS, KC_EQL, KC_HOME, KC_PGUP,
+  KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,                 KC_Y,     KC_U,    KC_I,    KC_O,   KC_END,  KC_PGDN,
+  MO(3),   KC_A, KC_S, KC_D, KC_F, KC_G,                 KC_H,     KC_J,    KC_K,    KC_L,   KC_P,    KC_BSPC,
+  SC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_F11, KC_F12, KC_N,     KC_M,    KC_COMM, KC_DOT, KC_SLSH, SC_RSPC,
+ KC_LCTL, KC_LGUI, KC_LALT, KC_ENT, MO(2),          MO(2), KC_SPC, KC_LBRC, KC_RBRC, KC_RCTL
 ),
 /*
  * COLEMAK-DH
